008682f4edf4139bff952ce037c5f43e2ee52bbe
jasper <whiteana001@warren.k12.in.us>
2024-08-05 12:58:33 -0400

    code

--- src/Prims2.java
+++ src/Prims2.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Prims2 {
+public class Prims2 {
+    public static void main(String[] args) {
+
+        byte myMinByteValue = -128;
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        int myNewInt = myMinByteValue;
+
+        float myFloat = 23.234f;
+        float myFloat2 = 1.4e-3F;
+        System.out.println(myFloat);
+        System.out.println(myFloat2);
+
+        double myDouble = -78.78787878;
+        double myDouble2 = 1.8e14;
+
+        System.out.println(myDouble);
+        System.out.println(myDouble2);
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+
+    }
 }
